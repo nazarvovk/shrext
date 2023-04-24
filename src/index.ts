c4613b5cfd4bea01d2daa917ae4b9e4ea27644e6
Nazar Vovk <nazarvovk.work@gmail.com>
2023-04-24 10:22:37 +0300

    feat: first

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import type { AfterMiddleware, BeforeMiddleware, NextContextOf, ShrextHandler, SupportedNextFunctions } from './types'
+
+export * from './types'
+
+export const shrext = <T extends SupportedNextFunctions, TMiddlewareContext = object>(
+  handler: (ctx: NextContextOf<T>, ShrextContext: TMiddlewareContext) => ReturnType<T>,
+): ShrextHandler<T, TMiddlewareContext> => {
+  const beforeMiddlewares: BeforeMiddleware<T, TMiddlewareContext>[] = []
+  const afterMiddlewares: AfterMiddleware<T, TMiddlewareContext>[] = []
+  const shrextHandler: ShrextHandler<T, TMiddlewareContext> = async (nextCtx: NextContextOf<T>) => {
+    const middlewareContext = {} as TMiddlewareContext
+    for (const beforeMiddleware of beforeMiddlewares) {
+      const result = await beforeMiddleware(nextCtx, middlewareContext)
+      if (result) return result
+    }
+    let result = await handler(nextCtx, middlewareContext)
+    for (const afterMiddleware of afterMiddlewares) {
+      result = await afterMiddleware(result, middlewareContext, nextCtx)
+    }
+    return result
+  }
+  shrextHandler.use = (middleware) => {
+    const { before, after } = middleware
+    if (before) shrextHandler.before(before)
+    if (after) shrextHandler.after(after)
+    return shrextHandler
+  }
+  shrextHandler.before = (beforeMiddleware) => {
+    beforeMiddlewares.push(beforeMiddleware)
+    return shrextHandler
+  }
+  shrextHandler.after = (afterMiddleware) => {
+    afterMiddlewares.unshift(afterMiddleware)
+    return shrextHandler
+  }
+  return shrextHandler
+}
+
+export default shrext
---
+++ src/types.ts
@@ -0,0 +1,44 @@
+
+import type { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next'
+
+export type SupportedNextFunctions = GetServerSideProps | GetStaticPaths | GetStaticProps
+
+export type NextContextOf<T extends SupportedNextFunctions> = Parameters<T>[0]
+
+export type BeforeMiddleware<
+  T extends SupportedNextFunctions = SupportedNextFunctions,
+  TShrextContext = object,
+> = (
+  nextContext: NextContextOf<T>,
+  ShrextContext: TShrextContext,
+) => void | Promise<void> | ReturnType<T>
+
+export type AfterMiddleware<
+  T extends SupportedNextFunctions = SupportedNextFunctions,
+  TShrextContext = object,
+> = (
+  prevResult: Awaited<ReturnType<T>>,
+  ShrextContext: TShrextContext,
+  nextContext: NextContextOf<T>,
+) => ReturnType<T> | Promise<ReturnType<T>>
+
+export type MiddlewareObject<
+  T extends SupportedNextFunctions = SupportedNextFunctions,
+  TShrextContext = object,
+> = {
+  before?: BeforeMiddleware<T, TShrextContext>
+  after?: AfterMiddleware<T, TShrextContext>
+}
+
+export type ShrextHandler<TFunction extends SupportedNextFunctions, TShrextContext = object> = {
+  (ctx: NextContextOf<TFunction>): ReturnType<TFunction> | Promise<ReturnType<TFunction>>
+  use: (
+    middlewareObject: MiddlewareObject<TFunction, TShrextContext>,
+  ) => ShrextHandler<TFunction, TShrextContext>
+  before: (
+    beforeMiddleware: BeforeMiddleware<TFunction, TShrextContext>,
+  ) => ShrextHandler<TFunction, TShrextContext>
+  after: (
+    afterMiddleware: AfterMiddleware<TFunction, TShrextContext>,
+  ) => ShrextHandler<TFunction, TShrextContext>
+}
