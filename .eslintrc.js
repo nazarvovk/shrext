a706bf14304f9a645944ef18c8cdc1002b6110bd
Nazar Vovk <nazarvovk.work@gmail.com>
2023-04-24 10:21:55 +0300

    feat: setup

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'lf',
+        printWidth: 100,
+        semi: false,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-unused-vars': 'off',
+    'no-debugger': 'error',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
+}
