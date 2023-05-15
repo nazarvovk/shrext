d49b6e99391a9d77c62fc6a38de38c77154acbcb
Nazar Vovk <nazarvovk.work@gmail.com>
2023-05-15 19:31:49 +0300

    chore: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,15 @@
-const { defaults: tsjPreset } = require('ts-jest/presets');
-
 module.exports = {
   transform: {
-    ...tsjPreset.transform,
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.test.json',
+        // Disable type checking for tests
+        isolatedModules: true,
+      },
+    ],
   },
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  roots: ['<rootDir>/src', '<rootDir>/test'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-      // Disable type checking for tests
-      isolatedModules: true,
-    },
-  },
+  roots: ['<rootDir>/src'],
 };
